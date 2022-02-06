e6f895ef4118e8fd2814c9aef22ca388e766ab7a
Yemi Gabriel <yemigabriel@gmail.com>
2022-02-06 18:22:21 +0100

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Yemi Gabriel on 2/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
